ddc95a3d245ddc7dba5013b3e1d4467a86d671d8
FabulousCupcake <mail.fabulous.cupcake@gmail.com>
2021-11-07 21:51:40 +0100

    Update full-logger plugin

--- app/plugins/full-logger.js
+++ app/plugins/full-logger.js
@@ -14,9 +14,15 @@ module.exports = {
   pluginName: 'FullLogger',
   pluginDescription: 'Dumps data for every API event into a file.',
   init(proxy, config) {
-    proxy.on('apiCommand', (req, resp) => {
+    proxy.on('debug', (endpoint, req, res) => {
       if (config.Config.Plugins[this.pluginName].enabled) {
-        this.logCommand(req, resp);
+        this.logCommand(endpoint, req, res);
+        proxy.log({
+          type: 'debug',
+          source: 'plugin',
+          name: this.pluginName,
+          message: `${endpoint}: sent ${req.length} / recv ${res.length}`,
+        });
       }
     });
     app.on('will-quit', () => {
@@ -25,31 +31,24 @@ module.exports = {
       }
     });
   },
-  logCommand(req, resp) {
-    const { command } = req;
-
+  logCommand(endpoint, req, res) {
     let logfile = fs.createWriteStream(path.join(config.Config.App.filesPath, 'full_log.txt'), {
       flags: 'a',
       autoClose: true,
     });
 
-    logfile.write(
-      `API Command: ${command}`.concat(
-        ' - ',
-        Date(),
-        eol,
-        'Request: ',
-        eol,
-        JSON.stringify(req),
-        eol,
-        'Response: ',
-        eol,
-        JSON.stringify(resp),
-        eol,
-        eol
-      )
-    );
-
+    logfile.write(`
+=============================================
+${endpoint} - ${Date()}
+--» SEND «-----------------------------------
+${JSON.stringify(btoa(req))}
+--» RECV «-----------------------------------
+${JSON.stringify(btoa(res))}
+=============================================`);
     logfile.end();
   },
 };
+
+function btoa(shit) {
+  return Buffer.from(shit).toString('base64');
+}

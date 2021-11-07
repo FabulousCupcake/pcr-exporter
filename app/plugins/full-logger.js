872607430ae4ed8bef94babf7a963e45146260b4
FabulousCupcake <mail.fabulous.cupcake@gmail.com>
2021-11-07 22:59:52 +0100

    Reformat Full Logger plugin

--- app/plugins/full-logger.js
+++ app/plugins/full-logger.js
@@ -3,41 +3,25 @@ const fs = require('fs');
 const path = require('path');
 const eol = require('os').EOL;
 
-module.exports = {
-  defaultConfig: {
-    enabled: false,
-    deleteFileOnQuit: false,
-  },
-  defaultConfigDetails: {
-    deleteFileOnQuit: { label: 'Delete log file before quitting app' },
-  },
-  pluginName: 'FullLogger',
-  pluginDescription: 'Dumps data for every API event into a file.',
-  init(proxy, config) {
-    proxy.on('debug', (endpoint, req, res) => {
-      if (config.Config.Plugins[this.pluginName].enabled) {
-        this.logCommand(endpoint, req, res);
-        proxy.log({
-          type: 'debug',
-          source: 'plugin',
-          name: this.pluginName,
-          message: `${endpoint}: sent ${req.length} / recv ${res.length}`,
-        });
-      }
-    });
-    app.on('will-quit', () => {
-      if (config.Config.Plugins[this.pluginName].deleteFileOnQuit) {
-        fs.unlinkSync(path.join(config.Config.App.filesPath, 'full_log.txt'));
-      }
-    });
-  },
-  logCommand(endpoint, req, res) {
-    let logfile = fs.createWriteStream(path.join(config.Config.App.filesPath, 'full_log.txt'), {
-      flags: 'a',
-      autoClose: true,
-    });
+// Plugin Metadata
+const pluginName = 'Full Logger';
+const pluginDescription = 'Dumps data for every API event into a file';
+const defaultConfig = {
+  enabled: false,
+  deleteFileOnQuit: false,
+};
+const defaultConfigDetails = {
+  deleteFileOnQuit: { label: 'Delete log file before quitting app' },
+};
 
-    logfile.write(`
+// Actual plugin starts here
+const log = (endpoint, req, res) => {
+  let logfile = fs.createWriteStream(path.join(config.Config.App.filesPath, 'full_log.txt'), {
+    flags: 'a',
+    autoClose: true,
+  });
+
+  logfile.write(`
 =============================================
 ${endpoint} - ${Date()}
 --» SEND «-----------------------------------
@@ -45,8 +29,38 @@ ${JSON.stringify(btoa(req))}
 --» RECV «-----------------------------------
 ${JSON.stringify(btoa(res))}
 =============================================`);
-    logfile.end();
-  },
+  logfile.end();
+};
+
+const debugHandler = (endpoint, req, res) => {
+  if (config.Config.Plugins[pluginName].enabled) {
+    log(endpoint, req, res);
+    proxy.log({
+      type: 'debug',
+      source: 'plugin',
+      name: pluginName,
+      message: `${endpoint}: sent ${req.length} / recv ${res.length}`,
+    });
+  }
+};
+
+const quitHandler = () => {
+  if (config.Config.Plugins[pluginName].deleteFileOnQuit) {
+    fs.unlinkSync(path.join(config.Config.App.filesPath, 'full_log.txt'));
+  }
+};
+
+const init = (proxy, config) => {
+  proxy.on('debug', debugHandler);
+  app.on('will-quit', quitHandler);
+};
+
+module.exports = {
+  defaultConfig,
+  defaultConfigDetails,
+  pluginName,
+  pluginDescription,
+  init,
 };
 
 function btoa(shit) {

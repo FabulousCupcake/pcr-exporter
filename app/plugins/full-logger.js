0adefad65b2fb1d2fef07c90c49c7501a9a9a1ec
FabulousCupcake <mail.fabulous.cupcake@gmail.com>
2021-11-08 01:18:32 +0100

    Add decryption capability to full logger

--- app/plugins/full-logger.js
+++ app/plugins/full-logger.js
@@ -1,7 +1,8 @@
 const { app } = require('electron');
 const fs = require('fs');
 const path = require('path');
-const eol = require('os').EOL;
+const aesjs = require('aes-js');
+const msgpack5 = require('msgpack5');
 
 // Plugin Metadata
 const pluginName = 'Full Logger';
@@ -9,14 +10,21 @@ const pluginDescription = 'Dumps data for every API event into a file';
 const defaultConfig = {
   enabled: false,
   deleteFileOnQuit: false,
+  dumpRaw: false,
+  logDecryptedRequest: false,
 };
 const defaultConfigDetails = {
   deleteFileOnQuit: { label: 'Delete log file before quitting app' },
+  dumpRaw: { label: 'Always dump raw, undecrypted data' },
+  logDecryptedRequest: { label: 'Only log decrypted response body' },
 };
 
+const RAW_LOG_FILENAME = 'raw_log.txt';
+const DECRYPTED_LOG_FILENAME = 'log.txt';
+
 // Actual plugin starts here
-const log = (endpoint, req, res) => {
-  let logfile = fs.createWriteStream(path.join(config.Config.App.filesPath, 'full_log.txt'), {
+const log = (filename, endpoint, req, res) => {
+  const logfile = fs.createWriteStream(path.join(config.Config.App.filesPath, filename), {
     flags: 'a',
     autoClose: true,
   });
@@ -25,22 +33,73 @@ const log = (endpoint, req, res) => {
 =============================================
 ${endpoint} - ${Date()}
 --» SEND «-----------------------------------
-${JSON.stringify(btoa(req))}
+${JSON.stringify(req)}
 --» RECV «-----------------------------------
-${JSON.stringify(btoa(res))}
+${JSON.stringify(res)}
 =============================================`);
   logfile.end();
 };
 
-const debugHandler = (endpoint, req, res) => {
-  if (config.Config.Plugins[pluginName].enabled) {
-    log(endpoint, req, res);
+// Decrypt attempts to decrypt the traffic with the obtained
+const decrypt = data => {
+  const aesBody = data.subarray(0, data.length - 32);
+  const aesKey = data.subarray(data.length - 32, data.length);
+  const ivKey = Buffer.from(global.config.Config.Configuration.ivKey, "utf8");
+
+  const rawBody = new aesjs.ModeOfOperation.cbc(aesKey, ivKey).decrypt(aesBody);
+  const body = rawBody.subarray(0, rawBody.length - rawBody[rawBody.length - 1]);
+  const msgpack = msgpack5().decode(body);
+  return msgpack;
+};
+
+const logDecrypted = (endpoint, req, rawRes) => {
+  const res = Buffer.from(rawRes.toString('utf8'), 'base64');
+
+  try {
+    const reqBody = decrypt(req);
+    if (global.config.Config.Plugins.logDecryptedRequest) {
+      proxy.log({
+        type: 'info',
+        source: 'plugin',
+        name: pluginName,
+        message: `send: <Segment piled><code>${endpoint}</code><pre>${JSON.stringify(reqBody)}</pre></Segment>`,
+      });
+    }
+
+    const resBody = decrypt(res);
     proxy.log({
-      type: 'debug',
+      type: 'info',
       source: 'plugin',
       name: pluginName,
-      message: `${endpoint}: sent ${req.length} / recv ${res.length}`,
+      message: `recv: <code>${endpoint}</code><pre>${JSON.stringify(resBody)}</pre>`,
     });
+    log(DECRYPTED_LOG_FILENAME, endpoint, reqBody, resBody)
+  } catch (err) {
+    proxy.log({
+      type: 'error',
+      source: 'plugin',
+      name: pluginName,
+      message: `Failed decrypting ${endpoint}: ${err}`,
+    });
+  }
+};
+
+const debugHandler = (endpoint, req, res) => {
+  if (config.Config.Plugins[pluginName].enabled) {
+    log(RAW_LOG_FILENAME, endpoint, req, res);
+
+    if (global.config.Config.Preferences.debug) {
+      proxy.log({
+        type: 'debug',
+        source: 'plugin',
+        name: pluginName,
+        message: `${endpoint}: sent ${req.length} / recv ${res.length}`,
+      });
+    }
+
+    if (global.config.Config.Plugins.dumpRaw) return;
+    if (!global.config.Config.Configuration.ivKey) return;
+    logDecrypted(endpoint, req, res);
   }
 };
 

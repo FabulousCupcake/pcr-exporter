4b56ebc5d3069647e7fbee303932bd74853637b6
FabulousCupcake <mail.fabulous.cupcake@gmail.com>
2022-01-20 02:06:34 +0100

    Retain target ranks of unowned units (#27)
    * Retain target ranks of unowned units

--- app/plugins/armory-exporter.js
+++ app/plugins/armory-exporter.js
@@ -64,6 +64,21 @@ const transformUnitList = (units) => {
     t: targetData[convertId(u.id)] || false,
   }));
 
+  // Find all target units the user does not own
+  // These should be retained, with the unit data set to all minimum (rank 1, level 1, …)
+  Object.keys(targetData)
+    .filter((t) => !result.find((u) => u.u == t))
+    .forEach((t) => {
+      result.push({
+        u: t,
+        e: '000000',
+        r: 3,
+        p: 1,
+        q: 0,
+        t: targetData[t],
+      });
+    });
+
   return result;
 };
 
